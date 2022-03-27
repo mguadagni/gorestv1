97ab225f3b74561cc0d782e17f12f200dad3a03f
mguadagni <guadagnimichael@gmail.com>
2022-03-27 16:44:55 -0400

    Created a Post and Put request

--- src/main/java/com/careerdevs/gorestv1/controllers/UserController.java
+++ src/main/java/com/careerdevs/gorestv1/controllers/UserController.java
@@ -1,12 +1,11 @@
 package com.careerdevs.gorestv1.controllers;
 
+import com.careerdevs.gorestv1.models.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -30,22 +29,16 @@ public class UserController {
 
         try {
             String url = "https://gorest.co.in/public/v2/users/" + userId;
+            String apiToken = env.getProperty("GOREST_TOKEN");
+            url += "?access-token=" + apiToken;
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setBearerAuth(env.getProperty("GOREST_TOKEN"));
-            HttpEntity request = new HttpEntity(headers);
-
-            return restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    request,
-                    Object.class
-            );
+            return restTemplate.getForObject(url, Object.class);
 
         } catch (Exception exception){
-            return "404: No user exists with the ID " + userId;
-        }
 
+            return "404: No user exists with the ID " + userId;
+
+        }
 
     }
 
@@ -78,6 +71,90 @@ public class UserController {
             return exception.getMessage();
         }
 
+    }
+
+    @PostMapping ("/qp")
+    public Object postUserQueryParam (
+            @RequestParam ("name") String name,
+            @RequestParam ("email") String email,
+            @RequestParam ("gender") String gender,
+            @RequestParam ("status") String status,
+            RestTemplate restTemplate
+
+    ) {
+
+        try {
+
+            String url = "https://gorest.co.in/public/v2/users/";
+            String token = env.getProperty("GOREST_TOKEN");
+            url += "?access-token=" + token;
+
+            UserModel newUser = new UserModel (name, email, gender, status);
+
+            System.out.println("Data to be sent:\n" + newUser);
+
+            HttpEntity<UserModel> request = new HttpEntity<>(newUser);
+
+            return restTemplate.postForEntity(url, request, UserModel.class);
+
+        } catch (Exception exception) {
+            System.out.println(exception.getClass());
+            return exception.getMessage();
+        }
+
+    }
+
+    @PostMapping ("/")
+    public ResponseEntity postUser (
+            RestTemplate restTemplate,
+            @RequestBody UserModel newUser
+    ) {
+        try {
+
+            String url = "https://gorest.co.in/public/v2/users/";
+            String token = env.getProperty("GOREST_TOKEN");
+            url += "?access-token=" + token;
+
+            HttpEntity<UserModel> request = new HttpEntity<>(newUser);
+
+            return restTemplate.postForEntity(url, request, UserModel.class);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass() + " \n " + e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping ("/")
+    public ResponseEntity putUser (
+            RestTemplate restTemplate,
+            @RequestBody UserModel updateData
+    ) {
+        try {
+
+            String url = "https://gorest.co.in/public/v2/users/" + updateData.getId();
+            String token = env.getProperty("GOREST_TOKEN");
+            url += "?access-token=" + token;
+
+            HttpEntity<UserModel> request = new HttpEntity<>(updateData);
+
+            ResponseEntity<UserModel> response = restTemplate.exchange(
+                    url,
+                    HttpMethod.PUT,
+                    request,
+                    UserModel.class
+            );
+
+            return new ResponseEntity(response.getBody(), HttpStatus.OK);
+
+        } catch(HttpClientErrorException.UnprocessableEntity e) {
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass() + " \n " + e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
---
+++ src/main/java/com/careerdevs/gorestv1/models/UserModel.java
@@ -0,0 +1,52 @@
+package com.careerdevs.gorestv1.models;
+
+public class UserModel {
+
+    private int id;
+
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    public UserModel() {
+    }
+
+    public UserModel(String name, String email, String gender, String status) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
